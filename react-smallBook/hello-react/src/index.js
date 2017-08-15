d92f448e704d8b73210cccd1569103991ee08044
hesy <heshy@caibab.com>
2017-08-15 18:41:19 +0800

    react练习

--- react-smallBook/hello-react/src/index.js
+++ react-smallBook/hello-react/src/index.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React ,{Component} from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+class Header extends Component {
+  render () {
+    return (
+      <div>
+        <h1>React 小书</h1>
+      </div>
+    )
+  }
+}
+
+
+
 ReactDOM.render(
-  <App />,
+  <Header />,
   document.getElementById('root')
 );
