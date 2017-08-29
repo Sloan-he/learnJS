378709c978fba3a8ae03bb0286bfd99d4cb10eee
hesy <heshy@caibab.com>
2017-08-29 18:12:17 +0800

    learn react

--- react-smallBook/hello-react/src/index.js
+++ react-smallBook/hello-react/src/index.js
@@ -1,21 +1,50 @@
 import React ,{Component} from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+//import App from './App';
 import './index.css';
 
-class Header extends Component {
+class LikeButton extends Component {
+  constructor () {
+    super()
+    this.state = {
+      isLiked: false,
+      count:0
+    }
+  }
+  handleClickOnLikeButton () {
+    //this.setState({
+    //  isLiked: !this.state.isLiked
+    //})
+    this.setState((prevState) => {
+      return prevState.count+=1
+    })
+
+    this.setState((prevState) => {
+      return prevState.count+=1
+    })
+    this.setState((prevState) => {
+      return prevState.count+=1
+    })
+  }
   render () {
+    console.log(this.state)
     return (
-      <div>
-        <h1>React 小书</h1>
-      </div>
+      <button onClick={this.handleClickOnLikeButton.bind(this)}>
+        {this.state.isLiked ? '取消' : '点赞'}{this.state.count} 👍
+      </button>
     )
   }
 }
 
-
+class Index extends Component {
+  render() {
+    return (
+      <LikeButton />
+    )
+  }
+}
 
 ReactDOM.render(
-  <Header />,
+  <Index />,
   document.getElementById('root')
 );
