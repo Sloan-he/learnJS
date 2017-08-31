cc2cd516e3d491753233447f74d4384761107cdc
hesy <heshy@caibab.com>
2017-08-31 18:01:28 +0800

    learn react

---
+++ react-smallBook/comment-app/src/Comment.js
@@ -0,0 +1,18 @@
+import React ,{Component} from 'react';
+//import App from './App';
+import './index.css';
+
+export default class Comment extends Component {
+  render () {
+    return (
+      <div className='comment'>
+        <div className='comment-user'>
+          <span>{this.props.comment.username} </span>：
+        </div>
+        <p>{this.props.comment.content}</p>
+      </div>
+    )
+  }
+}
+
+
--- react-smallBook/comment-app/src/index.js
+++ react-smallBook/comment-app/src/index.js
@@ -1,44 +1,9 @@
-import React ,{Component} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-//import App from './App';
 import './index.css';
-
-class LikeButton extends Component {
-  constructor () {
-    super()
-    this.state = {
-      isLiked: false,
-      count:0
-    }
-  }
-  static defaultProps = {
-    likedText:'取消',
-    unlikedText:'点赞'
-  }
-  handleClickOnLikeButton () {
-    this.setState({isLiked:!this.state.isLiked})
-  }
-  render () {
-    return (
-      <button onClick={this.handleClickOnLikeButton.bind(this)}>
-        {this.state.isLiked ? this.props.likedText : this.props.unlikedText}
-      </button>
-    )
-  }
-}
-
-
-class Index extends Component {
-  render() {
-    return (
-      <div>
-        <LikeButton unlikedText={'赞'} />
-      </div>
-    )
-  }
-}
+import CommentApp from './CommentApp'
 
 ReactDOM.render(
-  <Index />,
+  <CommentApp />,
   document.getElementById('root')
 );
