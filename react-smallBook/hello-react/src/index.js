4bb8baa0ae5b8f7402ed7aca134789b84ba0ce8c
hesy <240543051@qq.com>
2017-09-10 23:58:13 +0800

    learn react and learn less

--- react-smallBook/hello-react/src/index.js
+++ react-smallBook/hello-react/src/index.js
@@ -2,6 +2,7 @@ import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types'
 import './index.css';
+import Header from './Header'
 
 function createStore (reducer) {
   let state = null
@@ -32,31 +33,6 @@ const store = createStore(themeReducer)
 
 
 
-class Header extends Component {
-  static contextTypes = {
-    store: PropTypes.object
-  }
-  constructor () {
-    super()
-    this.state = { themeColor: '' }
-  }
-  componentWillMount () {
-    this._updateThemeColor()
-    const {store} = this.context
-    store.subscribe(() => this._updateThemeColor())
-  }
-
-  _updateThemeColor () {
-    const { store } = this.context
-    const state = store.getState()
-    this.setState({ themeColor: state.themeColor })
-  }
-  render () {
-    return (
-        <h1 style={{ color: this.state.themeColor }}>React.js 小书</h1>
-    )
-  }
-}
 
 
 class ThemeSwitch extends Component {
@@ -141,6 +117,10 @@ class Index extends Component {
     )
   }
 }
+
+
+
+
 
 ReactDOM.render(
   <Index />,
