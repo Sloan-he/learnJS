8273cc2203acebba8a032ab9f7dcdd56595ba200
hesy <240543051@qq.com>
2017-10-12 23:55:11 +0800

    learn node

--- nodeBy7Day/static.js
+++ nodeBy7Day/static.js
@@ -7,33 +7,13 @@ var MIME = {
   '.js': 'application/javascript'
 };
 
-function combineFiles(pathnames, callback) {
-  var output = [];
-
-  (function next(i, len) {
-    if (i < len) {
-      fs.readFile(pathnames[i], function (err, data) {
-        if (err) {
-          callback(err);
-        } else {
-          console.log(data)
-          output.push(data);
-          next(i + 1, len);
-        }
-      });
-    } else {
-      callback(null, Buffer.concat(output));
-    }
-  }(0, pathnames.length));
-}
-
 function main(argv) {
   var config = JSON.parse(fs.readFileSync(argv[0], 'utf-8')),
-    root = config.root || '.',
-    port = config.port || 80;
+      root = config.root || '.',
+      port = config.port || 80;
   http.createServer(function (request, response) {
     var urlInfo = parseURL(root, request.url);
-    combineFiles(urlInfo.pathnames, function (err, data) {
+    validateFiles(urlInfo.pathnames, function (err, data) {
       if (err) {
         response.writeHead(404);
         response.end(err.message);
@@ -41,12 +21,33 @@ function main(argv) {
         response.writeHead(200, {
           'Content-Type': urlInfo.mime
         });
-        console.log('cb data',data)
-        response.end(data);
+        outputFiles(urlInfo.pathnames, response);
       }
     });
   }).listen(port);
 }
+
+
+
+function validateFiles(pathnames, callback) {
+  (function next(i, len) {
+    if(i<len){
+      fs.stat(pathnames[i],function(err,stats){
+        if(err){
+          callback(err);
+        }else if(!stats.isFile()){
+          callback(new Error())
+        }else{
+          next(i + 1, len)
+        }
+      })
+    }else{
+      callback(null, pathnames);
+    }
+  }(0, pathnames.length));
+}
+
+
 
 function parseURL(root, url) {
   var base, pathnames, parts;
@@ -68,5 +69,22 @@ function parseURL(root, url) {
     pathnames: pathnames
   };
 }
+
+function outputFiles(pathnames, writer) {
+  (function next(i, len) {
+    if (i < len) {
+      var reader = fs.createReadStream(pathnames[i]);
+
+      reader.pipe(writer, { end: false });
+      reader.on('end', function() {
+        next(i + 1, len);
+      });
+    } else {
+      writer.end();
+    }
+  }(0, pathnames.length));
+}
+
+
 
 main(process.argv.slice(2));
