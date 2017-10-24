ced8e6ba267c2576d65bd8a45df67767980e9aad
hesy <240543051@qq.com>
2017-10-24 23:49:21 +0800

    learn node

---
+++ teachingNotInclud/lesson7/vendor/tests.js
@@ -0,0 +1,12 @@
+var should = chai.should();
+describe('simple test', function () {
+    it('should equal 0 when n === 0', function () {
+        window.fibonacci(0).should.equal(0);
+    });
+    it('should equal 1 when n === 1', function () {
+        window.fibonacci(1).should.equal(1);
+    });
+    it('should equal 55 when n === 10', function () {
+        window.fibonacci(10).should.equal(55);
+    });
+});
