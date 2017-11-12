f511a1ae687bd9b5dbcae32c284ea7f3b53e96a4
hesy <240543051@qq.com>
2017-11-13 00:08:42 +0800

    learn node

--- teachingNotInclud/testServer/app.js
+++ teachingNotInclud/testServer/app.js
@@ -2,21 +2,43 @@
  * Created by Admin on 2017/11/5.
  */
 var express = require('express')
+var fs = require('fs')
+var session = require('express-session')
 app = express()
-var qs = require('qs')
 app.use(express.static('./'));
 app.use('/assets',express.static('images'));
-var fs = require('fs')
+app.use(session({
+    secret: 'recommand 128 bytes random string',
+    cookie: { maxAge: 3600 * 1000}
+}))
 
-app.get('/root',function(req,res){
-    if(req.query.user === '桂林米粉' && req.query.pwd === 'guilinmifen'){
+app.get('/user',function(req,res){
+    if(req.query.user === '牛河' && req.query.pwd === 'niuhe'){
+        req.session.root = true
         fs.readFile('girl.json',function(err,data){
             if(!err){
-                res.status(200).send({list:data.toString(),ip:'http://47.92.101.29'})
+                res.status(200).send(data.toString())
                 return
             }
-        res.status(500).send('出错了！')
+        res.send('出错了！')
         })
+    }else{
+        res.send('账号或者密码错误！')
+    }
+})
+
+app.get('/check',function(req,res){
+    console.log(req.session)
+    if(req.session.root){
+        fs.readFile('girl.json',function(err,data){
+            if(!err){
+                res.status(200).send(data.toString())
+                return
+            }
+            res.send('出错了！')
+        })
+    }else{
+        res.send('请登录')
     }
 })
 
