552bcc182e6a76cf3cb1b78f538f0b2ca5007117
hesy <240543051@qq.com>
2018-03-04 00:01:53 +0800

    learn webpack

---
+++ webpack/webpack.config.js
@@ -0,0 +1,31 @@
+/**
+ * Created by Admin on 2018/3/3.
+ */
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+    entry:{
+        app: './src/index.js'
+    },
+    devtool:'inline-source-map',
+    devServer: {
+        contentBase: './dist',
+        hot: true
+    },
+    plugins:[
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            title: 'Output Management'
+        }),
+        new webpack.NamedModulesPlugin(),
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    output:{
+        filename:'[name].bundle.js',
+        path:path.resolve(__dirname,'dist'),
+        //publicPath: '/'
+    }
+}
