40a59342b6c9f20c753c67172f5cdef1e3d1f259
hesy <240543051@qq.com>
2018-03-26 00:36:50 +0800

    learn webpack

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,31 +1,57 @@
-/**
- * Created by Admin on 2018/3/3.
- */
 const path = require('path');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
+//生成全新的HTML文件
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+//清理/dist文件夹
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
 
 module.exports = {
     entry:{
-        app: './src/index.js'
+        app:'./src/index.js'
     },
-    devtool:'inline-source-map',
-    devServer: {
+    devtool: 'inline-source-map',
+    //开发服务器
+    devServer:{
         contentBase: './dist',
-        hot: true
+        hot:true,
+        host: 'localhost'
     },
     plugins:[
         new CleanWebpackPlugin(['dist']),
         new HtmlWebpackPlugin({
-            title: 'Output Management'
+            title:'Hot Module Management'
         }),
         new webpack.NamedModulesPlugin(),
         new webpack.HotModuleReplacementPlugin()
     ],
-    output:{
-        filename:'[name].bundle.js',
-        path:path.resolve(__dirname,'dist'),
-        //publicPath: '/'
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/'
     }
-}
+    //module:{
+    //    rules:[
+    //        {
+    //            test:/\.css$/,
+    //            use:[
+    //                'style-loader',
+    //                'css-loader'
+    //            ]
+    //        },
+    //        {
+    //            test:/\.(png|svg|jpg|gif)$/,
+    //            use:[
+    //                'file-loader'
+    //            ]
+    //        },
+    //        {
+    //            test:/\.(woff|woff2|eot|ttf|otf)$/,
+    //            use:[
+    //                'file-loader'
+    //            ]
+    //        }
+    //    ]
+    //}
+};
