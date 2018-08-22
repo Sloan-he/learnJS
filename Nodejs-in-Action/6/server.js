78dd051ca94af85a2aa5ebe099db497ebd6025b3
hesy <240543051@qq.com>
2018-08-23 00:08:29 +0800

    node.js实战学习

---
+++ Nodejs-in-Action/6/server.js
@@ -0,0 +1,22 @@
+const connect = require('connect')
+
+const app = connect()
+
+function logger(req,res,next){
+  console.log('%s %s',req.method,req.url)
+  next()
+}
+
+function hello(req,res){
+  res.setHeader('Content-Type','text/plain')
+  res.end('hello world')
+}
+
+app.use(logger)
+
+app.use(hello)
+
+
+
+
+app.listen(3000)
