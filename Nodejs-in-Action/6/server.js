7b5ad0bd37f66643496827f3775da75afad6e4c1
hesy <240543051@qq.com>
2018-08-24 00:19:27 +0800

    node.js实战学习

--- Nodejs-in-Action/6/server.js
+++ Nodejs-in-Action/6/server.js
@@ -1,10 +1,33 @@
 const connect = require('connect')
 
 const app = connect()
+let router = ('./router')
+let routes = {
+  GET:{
+    '/users':function(req,res){
+      res.end('tobi,loki,ferret')
+    },
+    '/user/:id':function(req,res,id){
+      res.end('user ' + id)
+    }
+  },
+  DELETE:{
+    '/user/:id':function(req,res,id){
+      res.end('deleted user ' + id)
+    }
+  }
+}
 
-function logger(req,res,next){
-  console.log('%s %s',req.method,req.url)
-  next()
+function loggerSetup(req,res,next){
+  let regexp = /:(\w+)/g
+
+  return function logger(req,res,next){
+    let str = format.replace(regexp,function(match,property){
+      return req[property]
+    })
+    console.log(str)
+    next()
+  }
 }
 
 function hello(req,res){
@@ -12,9 +35,9 @@ function hello(req,res){
   res.end('hello world')
 }
 
-app.use(logger)
+app.use(loggerSetup(':method :url'))
 
-app.use(hello)
+app.use(router( ))
 
 
 
